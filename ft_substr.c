9f0c1af4f535fd3b5b0efc685673a235dc2723a4
skihara <yaki_nigi_uma@yahoo.co.jp>
2020-12-01 12:35:02 +0900

    Fix substr if len size

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: skihara <skihara@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 12:34:57 by skihara           #+#    #+#             */
-/*   Updated: 2020/12/01 08:54:41 by skihara          ###   ########.fr       */
+/*   Updated: 2020/12/01 12:34:32 by skihara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (len <= 0 || ft_strlen(s) <= start)
-		return (ft_strdup(""));
+		return (ft_strdup_"");
+	if (len > (ft_strlen(s) - (size_t)start))
+		len = ft_strlen(s) - (size_t)start;
 	ans = (char*)malloc(sizeof(char) * (len + 1));
 	if (ans == NULL)
 		return (NULL);
