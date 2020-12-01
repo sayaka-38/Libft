f4d3f3b8ae3775b3cbc829aa3a9350d23cc99c26
skihara <yaki_nigi_uma@yahoo.co.jp>
2020-12-01 12:43:36 +0900

    Fix substr conditional made simple.

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: skihara <skihara@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 12:34:57 by skihara           #+#    #+#             */
-/*   Updated: 2020/12/01 12:34:32 by skihara          ###   ########.fr       */
+/*   Updated: 2020/12/01 12:39:12 by skihara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (len <= 0 || ft_strlen(s) <= start)
-		return (ft_strdup_"");
+		return (ft_strdup(""));
 	if (len > (ft_strlen(s) - (size_t)start))
 		len = ft_strlen(s) - (size_t)start;
 	ans = (char*)malloc(sizeof(char) * (len + 1));
 	if (ans == NULL)
 		return (NULL);
 	i = 0;
-	while (i < ft_strlen(s) && i < len)
+	while (i < len)
 	{
 		ans[i] = s[start + i];
 		i++;
